54cc78cb9415910ae7cfb1a2b22c60a9eef257be
dufranco <63911120+dufranco@users.noreply.github.com>
2025-08-29 23:10:22 -0300

    Adicionado script de inserção inicial

---
+++ script_tcc_lanchonete_inserts.sql
@@ -0,0 +1,7 @@
+INSERT INTO perfis VALUES (1, 'Admin', 'Administrador do sistema. Possui acesso a todas as telas.');
+INSERT INTO usuarios VALUES (1, 1, 'Administrador', 'edu.developer@outlook.com', 'xrPeJKBdRT5HDSmn3BPoJy6ia2Ike3NfBEBPDE5mOyQ=', 'RuKYnyXR1JlPU4ch4StNe7Avx2dFArSSR7cFChaVkBQ=');
+INSERT INTO controle_acesso VALUES (1, 1, 'cadastro_controleacessos', 'true');
+INSERT INTO controle_acesso VALUES (2, 1, 'cadastro_pedidos', 'true');
+INSERT INTO controle_acesso VALUES (3, 1, 'cadastro_perfis', 'true');
+INSERT INTO controle_acesso VALUES (4, 1, 'cadastro_produtos', 'true');
+INSERT INTO controle_acesso VALUES (5, 1, 'cadastro_usuarios', 'true');
